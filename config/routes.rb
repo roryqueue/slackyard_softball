0ee0157a214d5b8877331075ae2c3723c8950e78
Rory Quinlan <rjamesquinlan@gmail.com>
2014-07-07 18:23:23 -0400

    added initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :leagues, shallow: true do
+    resources :teams
+    resources :players
+    resources :games
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
