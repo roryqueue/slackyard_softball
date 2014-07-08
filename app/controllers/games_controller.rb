bf500d0e8886d52955ff1d90f8725078221bab94
Rory Quinlan <rjamesquinlan@gmail.com>
2014-07-07 22:15:20 -0400

    added show page for game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,9 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    @runs = @game.score_keeper.order("created_at DESC")
+    @home_pitcher = Game.home_team_lineup.pitcher
+    @away_pitcher = Game.away_team_lineup.pitcher
   end
 
 end
