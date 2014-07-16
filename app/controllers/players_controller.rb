152f96bcadfe8b3959b1d27d2bff066f8d831355
Rory Quinlan <rjamesquinlan@gmail.com>
2014-07-16 11:52:55 -0400

    added contact page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,4 +3,23 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
   end
+
+  def new
+    @team = Team.find(params[:team_id])
+    @player = Player.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    @team.user = current_user if current_user
+    @team.league = League.find(params[:league_id])
+
+    if @team.save
+      flash[:notice] = "Your team '#{@team.name}' has been created!"
+      redirect_to team_path(@team)
+    else
+      flash.now[:notice] = "Your team could not be created!"
+      render "teams#new"
+    end
+  end
 end
